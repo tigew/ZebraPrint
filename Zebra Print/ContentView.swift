201bd8fcf04e3154992115c5f96c30437330411b
Travis Peebles <travis@peebles.family>
2025-05-27 02:27:32 -0500

    Initial Commit

---
+++ Zebra Print/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zebra Print
+//
+//  Created by Travis Peebles on 5/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zebra Print/Zebra_PrintApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zebra_PrintApp.swift
+//  Zebra Print
+//
+//  Created by Travis Peebles on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Zebra_PrintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
